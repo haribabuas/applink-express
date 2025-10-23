fd7b519d095c1fe145992233af0cd3184912a53d
Jonathan Jenkins <jonathan.jenkins@salesforce.com>
2025-10-23 13:37:52 -0700

    collapsed getting salesforce connection from applink SDK to single line

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const PORT = process.env.APP_PORT || 3000
-const applinkSDK = require('@heroku/applink');
+const applinkSDK = require('@heroku/applink')
 const express = require('express')
 const app = express()
 
 app.use(express.json())
 
 app.get('/accounts', async (req, res) => {
-    const sf = getSalesforceConnection(req);
+    const sf = applinkSDK.parseRequest(req.headers, req.body, null).context.org.dataApi;
 
     const queryString = "SELECT Id, Name FROM Account LIMIT 10";
 
@@ -19,14 +19,3 @@ app.get('/accounts', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Listening on ${ PORT }`)
 })
-
-
-function getSalesforceConnection(request) {
-    const applink = applinkSDK.init();
-    const parsedRequest = applink.salesforce.parseRequest(
-        request.headers,
-        request.body,
-        null
-    );
-    return parsedRequest.context.org.dataApi;
-}
