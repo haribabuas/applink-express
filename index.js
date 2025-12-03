6204642bc82c051a6cf1e8315ac9444592f25ccc
haribabu <haribabu.adra.ext@siemens.com>
2025-12-03 17:23:18 -0500

    asda

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 
+const PORT = process.env.APP_PORT || 3000;
+const applinkSDK = require('@heroku/applink');
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+
 app.post('/api/generatequotelines', async (req, res) => {
   const { quoteId, sapLineIds } = req.body;
 
@@ -8,6 +15,7 @@ app.post('/api/generatequotelines', async (req, res) => {
 
     const sf = applinkSDK.parseRequest(req.headers, req.body, null);
     const org = sf.context.org;
+
     console.log('@@@Org Context:', org);
 
     const uow = org.dataApi.newUnitOfWork();
@@ -15,10 +23,14 @@ app.post('/api/generatequotelines', async (req, res) => {
 
     const commitResult = await org.dataApi.commitUnitOfWork(uow);
     const result = commitResult.getResult(refId);
-  console.log('@@@Org result:', result);
-   
+console.log('@@@Org result:', result);
     res.json({
       message: 'Account created successfully'
     });
-  
+ 
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
 });
