0b857326e84eeb48e00c58c98c80028a3c5961a7
haribabu <haribabu.adra.ext@siemens.com>
2025-12-02 22:27:16 -0500

    sdasa

--- index.js
+++ index.js
@@ -8,10 +8,17 @@ app.use(express.json())
 app.get('/accounts', async (req, res) => {
     console.log('@@@',req.body);
     console.log('@@@',req.headers);
-
+    
+    req.sdk = applinkSDK.init();
+    const { event, context, logger } = request.sdk;
+    const queryString = "SELECT Id, Name FROM Account LIMIT 10";
+    const org = context.org;
+    console.log('@@@org',org);
+    const result = await org.dataApi.query(query);
+    console.log('@@@result',result);
     const sf = applinkSDK.parseRequest(req.headers, req.body, null).context.org.dataApi;
     console.log('@@@sf',sf);
-    const queryString = "SELECT Id, Name FROM Account LIMIT 10";
+    
 
     const queryResult = await sf.query(queryString);
     const outAccounts = queryResult.records.map(rec => rec.fields);
