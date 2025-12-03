c5eeb0eb8c965bdd379617cba8b4df728ff9bf80
haribabu <haribabu.adra.ext@siemens.com>
2025-12-02 20:15:04 -0500

    serviceupdate

--- index.js
+++ index.js
@@ -16,6 +16,19 @@ app.get('/accounts', async (req, res) => {
     res.json(outAccounts);
 })
 
+app.post('/create-quote-lines-sap', async (req, res) => {
+    const { quoteId, sapLineIds } = req.body;
+  console.log('Incoming request body:', req.body);
+    console.log('@@@quoteId:', quoteId);
+  const sf = applinkSDK.parseRequest(req.headers, req.body, null).context.org.dataApi;
+
+    const queryString = "SELECT Id, Name FROM Account LIMIT 10";
+    const queryResult = await sf.query(queryString);
+    const outAccounts = queryResult.records.map(rec => rec.fields);
+    console.log('@@@outAccounts',outAccounts);
+
+})
+
 app.listen(PORT, () => {
     console.log(`Listening on ${ PORT }`)
 })
