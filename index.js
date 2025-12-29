8684638cca15be5b6a96d256ee7184f45303d873
haribabu <haribabu.adra.ext@siemens.com>
2025-12-29 17:43:47 -0500

    OrderApp

--- index.js
+++ index.js
@@ -5,30 +5,6 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-/*app.post('/test/generatequotelines', async (req, res) => {
-  const { quoteId, sapLineIds } = req.body;
-
-    const sf = applinkSDK.parseRequest(req.headers, req.body, null);
-    const org = sf.context.org;
-    const dataApi = sf.context.org.dataApi;
-    console.log('@@@Org Context:', dataApi);
-
-    const uow = dataApi.newUnitOfWork();
-    const accountId = uow.registerCreate({
-        type: 'Account',
-        fields: {
-          Name: 'Test Account',
-        },
-      });
-
-    const response = await dataApi.commitUnitOfWork(uow);
-    
-console.log('@@@Org result:', response);
-    res.json({
-      message: 'Account created successfully'
-    });
- 
-});*/
 
 function chunkArray(array, size) {
   const result = [];
