2deebe69df1fd77b6d31a7cc5c9996a640817c51
haribabu <haribabu.adra.ext@siemens.com>
2026-01-01 23:09:00 -0500

    sdsdsd

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.post('/api/generateOrderlines', async (req, res) => {
         SBQQ__EffectiveEndDate__c,
         SBQQ__NetPrice__c
       FROM SBQQ__QuoteLine__c
-      WHERE SBQQ__Quote__c = '${safeQuoteId}'
+      WHERE SBQQ__Quote__c = '${safeQuoteId}' limit 1
     `;
 
     const qResult = await dataApi.query(soql);
@@ -83,7 +83,6 @@ app.post('/api/generateOrderlines', async (req, res) => {
       });
     }
 
-    const fv = (record, fieldApiName) => record?.fields?.[fieldApiName]?.value;
 
     const buildOrderItemFields = (line) => {
       const rec = line?.fields;
