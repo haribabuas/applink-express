1e4bb67eb48ef2ed75cb14f0f3fd3cd7589b3f20
haribabuas <haribabu.adra.ext@siemens.com>
2026-01-05 22:07:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ app.post('/api/generateOrderlines', async (req, res) => {
           fields
         });
       }
-      console.log('@@@uow',uow);
+      //console.log('@@@uow',uow);
       results = await dataApi.commitUnitOfWork(uow);
       createdCount = quoteLines.length;
 
