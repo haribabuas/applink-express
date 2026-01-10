04e32fbe067646138d54e3e8101a0df5d34907a6
haribabuas <haribabu.adra.ext@siemens.com>
2026-01-10 16:45:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.post('/api/generateOrderlines', async (req, res) => {
       };
     };
 
-    const BATCH_SIZE = 500;
+    const BATCH_SIZE = 1000;
     const resultsPerBatch = [];
     let createdCount = 0;
 
